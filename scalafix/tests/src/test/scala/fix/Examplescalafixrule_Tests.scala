a81fb0a2e08ae8550733024517795ad6c4c2ea3a
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-08-03 12:00:31 +0200

    Upgrade to latest scalafix milestone

--- scalafix/tests/src/test/scala/fix/Examplescalafixrule_Tests.scala
+++ scalafix/tests/src/test/scala/fix/Examplescalafixrule_Tests.scala
@@ -2,14 +2,9 @@ package fix
 
 import scala.meta._
 import scalafix._
-import scalafix.testkit._
+import scalafix.v0._
 
 class Examplescalafixrule_Tests
-    extends SemanticRuleSuite(
-      SemanticdbIndex.load(
-        Classpath(AbsolutePath(BuildInfo.inputClassdirectory))),
-      AbsolutePath(BuildInfo.inputSourceroot),
-      Seq(AbsolutePath(BuildInfo.outputSourceroot))
-    ) {
+    extends scalafix.testkit.SemanticRuleSuite {
   runAllTests()
 }
